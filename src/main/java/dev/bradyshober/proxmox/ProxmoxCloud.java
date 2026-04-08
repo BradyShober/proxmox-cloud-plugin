3a6e941c784a1f0131bc984e3f7a12dfc451c5aa
Brady Shober <32466298+BradyShober@users.noreply.github.com>
2026-04-08 03:46:13 +0000

    Call super.readResolve()

--- src/main/java/dev/bradyshober/proxmox/ProxmoxCloud.java
+++ src/main/java/dev/bradyshober/proxmox/ProxmoxCloud.java
@@ -143,6 +143,7 @@ public class ProxmoxCloud extends Cloud {
 
     @Serial
     private Object readResolve() {
+        super.readResolve();
         proxmoxClient = null;
         startupReconciled = false;
         initTransientState();
